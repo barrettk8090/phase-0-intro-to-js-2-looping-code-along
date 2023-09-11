c71aedba9f99cfe048d06e44a9588d074bd44f74
Barrett Kowalsky <barrettkowalsky@gmail.com>
2023-09-11 12:48:21 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+const messages = [];
+
+function writeCards(names, eventName){
+    for (let i = 0; i < names.length; i++){
+      messages.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`)
+    }
+    return messages
+
+}
+
+function countDown(number){
+    while (number >= 0){
+        console.log(number--);
+    }
+}
